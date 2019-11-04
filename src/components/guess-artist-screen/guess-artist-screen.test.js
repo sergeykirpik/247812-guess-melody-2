8a2bb3792a0ef6cf5f1a667dc3eee659892f4520
Sergey Kirpik <sergeykirpik@gmail.com>
2019-11-04 19:37:54 +0200

    audio-player: part 01

--- src/components/guess-artist-screen/guess-artist-screen.test.js
+++ src/components/guess-artist-screen/guess-artist-screen.test.js
@@ -4,12 +4,21 @@ import GuessArtistScreen from './guess-artist-screen';
 
 import questions from '../../mocks/questions';
 
+const createNodeMock = (el) => {
+  if (el.type === `audio`) {
+    return {
+      src: ``,
+    };
+  }
+  return null;
+};
+
 it(`renders correctly`, () => {
   const question = questions[1];
   const tree = renderer
     .create(<GuessArtistScreen
       songSrc={question.src}
-      answers={question.answers} />)
+      answers={question.answers} />, {createNodeMock})
     .toJSON();
 
   expect(tree).toMatchSnapshot();
